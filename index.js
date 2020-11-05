694674994297068c363ad536e05f516f94a60ba7
MasterkinG32 <lichwow_masterking@yahoo.com>
2020-11-05 15:13:18 -0800

    First commit.

---
+++ index.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+	firstPlay = true;
+	const myAudio = document.getElementById("audio_play");
+	const myVideo = document.getElementById('video_intro');
+	myVideo.volume = 0.1;
+	document.onkeydown = function(event) {
+		switch (event.keyCode) {
+			case 32: 
+				myVideo.muted = !myVideo.muted;
+				if(myVideo.muted)
+				{
+					myAudio.innerHTML  = '[ Unmuted (Press space to mute) ]';
+				}else{
+					myAudio.innerHTML = '[ Muted (Press space to unmute) ]';
+				}
+				break;
+			case 107:
+				if(myVideo.volume <= 1)
+					myVideo.volume = myVideo.volume + 0.1
+				break;
+			case 109:
+				if(myVideo.volume > 0)
+					myVideo.volume = myVideo.volume - 0.1
+				break;
+		}
+		return false;
+	}
+})
